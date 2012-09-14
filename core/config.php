2efead3c8ca0b5b56349eb38b65e3e1d10c52a52
CreatureMyst <creaturemyst@gmail.com>
2012-09-14 17:06:11 +0700

    Update core/config.php

--- core/config.php
+++ core/config.php
@@ -1,7 +1,7 @@
 <?php
 $cMysql = array(
-    "host"  => "mysql0.db.koding.com",
-    "user"  => "creaturemyst_324",
-    "pass"  => "Your pass here",
-    "db"    => "creaturemyst_324"
+    "host"  => "",
+    "user"  => "",
+    "pass"  => "",
+    "db"    => ""
 );
